f6440f69a9c442fd672471c43b6556aec57dcadd
Pranto Deb <rajprit0m@outlook.com>
2025-05-27 19:54:29 +0600

    Tasks Code

--- day-1/06_task_code.c
+++ day-1/06_task_code.c
@@ -2,8 +2,35 @@
 
 int main()
 {
+    /**
+     * Task - 01 : Write a C program to store your name, age, and height in variables and print them using printf().
+     */
 
-    
+    // int age = 21;
+    // float height = 1.75;
+    // printf("I am %d years old.\nMy height is %.2f meters.\n\n\n", age, height);
+
+    /**
+     * Task - 02 : Write a C program to take two integer numbers as input and print their sum, difference, product, and division.
+     */
+
+    // int a, b;
+    // printf("Enter first number: ");
+    // scanf("%d", &a);
+
+    // printf("Enter second number: ");
+    // scanf("%d", &b);
+
+    // float div = a / b;
+
+    // printf("\nSum: %d \nDifference : %d \nProduct : %d \nDivision : %.2f", a + b, a - b, a * b, div);
+
+    /**
+     * Task - 03: Write a program to take your favorite quote as input (string) and print it. Use a char array for string input.
+     */
+
+    // char quote[] = "Work hard, dream big";
+    // printf("Your quote: %s", quote);
 
     return 0;
 }
